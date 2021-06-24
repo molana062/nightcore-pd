10344bc72b55557f347d09d6f30e2bf5bed62a0d
ipindanger <85965834+ipindanger@users.noreply.github.com>
2021-06-24 11:15:32 +0700

    Update sosmed.py

--- userbot/modules/sosmed.py
+++ userbot/modules/sosmed.py
@@ -44,7 +44,7 @@ async def insta(event):
             await event.client.send_file(
                 event.chat_id,
                 response.message.media,
-                caption=f"@Betterthaanhecan @ruangpublikk",
+                caption=f"@ygtagbayar @ipinUpcloud",
             )
             await event.client.send_read_acknowledge(conv.chat_id)
             await bot(functions.messages.DeleteHistoryRequest(peer=chat, max_id=0))
